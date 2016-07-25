5b90d62333f3bd2e43435ba9dadddb50fe8086eb
Maria Santamaria <maria.santamaria@hotmail.com>
2016-07-24 23:12:16 -0400

    Changed photos and added logo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,8 +22,15 @@
 
         // Background slideshow
         $.backstretch([
-            'assets/images/bg3.jpg', 
-            'assets/images/bg2.jpg', 
+            'assets/images/bg3.jpg',
+            'assets/images/bg11.jpg', 
+            'assets/images/bg2.jpg',
+            'assets/images/bg5.jpg', 
+            'assets/images/bg10.jpg',
+            'assets/images/bg6.jpg',
+            'assets/images/bg8.jpg',
+            'assets/images/bg7.jpg',
+            'assets/images/bg12.jpg',
             'assets/images/bg1.jpg'
         ], {duration: 3000, fade: 750});
 

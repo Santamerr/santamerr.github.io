506b1d302b123d889bb3584e1b490b0776d02d4a
Maria Santamaria <maria.santamaria@hotmail.com>
2016-08-02 16:09:27 -0400

    Added logos

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,10 @@
 	$(document).ready(function() {
 
         $('.your-class').slick({
+            dots: true,
             infinite: true,
             autoplay: true,
-            autoplaySpeed: 5000,
+            autoplaySpeed: 3000,
             slidesToShow: 4,
             slidesToScroll: 4
         });

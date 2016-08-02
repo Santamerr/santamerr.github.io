00d87fcf025c13b3f5fb2c19496722202404a6a1
Omas Abdullah <omas.abdullah@gmail.com>
2016-08-02 11:31:50 -0400

    Added arrows

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,8 @@
             autoplay: true,
             autoplaySpeed: 5000,
             slidesToShow: 4,
-            slidesToScroll: 4
+            slidesToScroll: 4,
+            arrows: true
         });
 
         // Animation scroll

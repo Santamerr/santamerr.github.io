7ddc0d130b1b260edb783e8f3991cdee9360a6fc
Maria Santamaria <maria.santamaria@hotmail.com>
2016-08-02 11:59:45 -0400

    Added more logos to carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,8 @@
             infinite: true,
             autoplay: true,
             autoplaySpeed: 5000,
-            slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToShow: 4,
+            slidesToScroll: 4
         });
 
         // Animation scroll

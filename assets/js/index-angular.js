0dda88eff37e261f86add3160f453e99c7f884bd
Omas Abdullah <omas.abdullah@gmail.com>
2016-08-19 01:51:53 -0400

    Removed image which is now hosted on firebase, and gif no longer needed. Added placeholder for preview carousel

--- assets/js/index-angular.js
+++ assets/js/index-angular.js
@@ -3,8 +3,7 @@ var app = angular.module("tobysList", ["firebase","slick"]);
 
 app.controller("mainController", function($scope, $firebaseObject, $firebaseArray) {
 	var ref = new Firebase("https://tobys-list-ed1c7.firebaseio.com");
-
-	$scope.test = ["1","2"];
 	
 	$scope.rescues = $firebaseArray(ref);
+	console.log($firebaseArray(ref));
 });
